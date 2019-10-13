83222140b78ee11b8928a22028183974a4766ee9
Prod <Prod@SYSTEM-72>
2019-10-13 10:13:53 +0530

    ok

--- LMS_Project/src/test/java/lms/test/LoginTest.java
+++ LMS_Project/src/test/java/lms/test/LoginTest.java
@@ -1,5 +1,7 @@
 package lms.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Listeners;
@@ -10,17 +12,26 @@ import lms.genericLibraries.BaseClass;
 @Listeners(lms.genericLibraries.ListenerImplemention.class)
 public class LoginTest extends BaseClass
 {
-
+ private static Logger log=LogManager.getLogger(LoginTest.class);
 	@Test
 	public void LoginTest()
 	{
     //Declaring the expected page title
-	Reporter.log("Login to NewTest is successful with admin account", true);
+    log.debug("*****Login page is displayed*****");
 	String expTitle="Employee Service Platform";
 	//printing the actual page title
 	String actTitle=driver.getTitle();
 	Assert.assertEquals(actTitle, expTitle);
-	Reporter.log(actTitle, true);
+	log.info("Title window is displayed :" +actTitle);
+	log.fatal("This is fatal");
+	
 	 }
+	
+ @Test()
+ public static void check()
+ {
+	 log.debug("Do some operations");
+	 log.info("This is just a test");
+ }
 
 }
